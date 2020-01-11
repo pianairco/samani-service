07e3adb4168957431718b34a16917d6a04c65d83
Mohammad Javad Rahmati <mj.rahmati@rha.local>
2020-01-11 14:59:51 +0330

    initial commit

---
+++ src/main/java/ir/piana/dev/samaniservice/SamaniServiceApplication.java
@@ -0,0 +1,13 @@
+package ir.piana.dev.samaniservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamaniServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamaniServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/piana/dev/samaniservice/SamaniServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.piana.dev.samaniservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamaniServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
